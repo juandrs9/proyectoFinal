0d6ea72c93e5d14b238db5bacfae4b168030774f
Felipe1218 <pipe.r6@hotmail.com>
2018-05-06 18:42:21 -0500

    se realizó cambio 2

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,11 @@ Rails.application.routes.draw do
   #Rails.application.routes.draw do
   #resources :users
   root   'products#home'
-  get    '/help',    to: 'static_pages#help'
-  get    '/about',   to: 'static_pages#about'
-  get    '/contact', to: 'static_pages#contact'
-  get    '/signup',  to: 'users#new'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  #get    '/help',    to: 'static_pages#help'
+  #get    '/about',   to: 'static_pages#about'
+  #get    '/contact', to: 'static_pages#contact'
+  #get    '/signup',  to: 'users#new'
+  #get    '/login',   to: 'sessions#new'
+  #post   '/login',   to: 'sessions#create'
+  #delete '/logout',  to: 'sessions#destroy'
 end
